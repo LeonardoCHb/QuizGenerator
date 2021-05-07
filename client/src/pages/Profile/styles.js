c85c415af4bb6d6befa0f80da4191fad340282ee
LeonardoCHb <leonardohably@hotmail.com>
2021-05-07 11:48:22 -0300

    some styles fixes

--- client/src/pages/Profile/styles.js
+++ client/src/pages/Profile/styles.js
@@ -2,14 +2,11 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
   paper: {
-    marginTop: theme.spacing(2),
-    marginBottom: theme.spacing(2),
-    marginLeft: theme.spacing(60),
-    marginRight: theme.spacing(60),
+    marginTop: theme.spacing(3),
     display: "flex",
+    margin: 270,
     flexDirection: "column",
     // alignItems: "center",
-    height: "20vh",
     padding: theme.spacing(2),
     /* background: "linear-gradient(to bottom, #0099ff 5%, #99ccff 86%)", */
   },
